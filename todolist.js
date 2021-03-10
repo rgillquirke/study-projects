a133cb10855bafe84311d13f6ca0fc90b5f73a0d
rgillquirke <73988648+rgillquirke@users.noreply.github.com>
2021-03-10 12:50:38 +0100

    Add files via upload
    Added projects in progress

---
+++ todolist.js
@@ -0,0 +1,28 @@
+let input = prompt("What would you like to do?");
+
+const todos = ["Collect Chicken Eggs", "Clean Cat Litter Box"]; 
+
+while (input !== "quit" && input !== "q"){
+    if (input ==="list"){
+        console.log("*************")
+        for (let i = 0; i < todos.length; i++){
+            console.log(`${i}: ${todos[i]}`);
+        }
+        console.log("*************")
+    } else if (input === "new") {
+        const newTodo = prompt("What is the new to do?");
+        todos.push(newTodo);
+        console.log(`${newTodo} added to the list!`)
+    } else if (input === "delete") {
+        const index = parseInt(prompt("Ok, enter an index to delete:"));
+        if (!Number.isNan(index)) {
+            const deleted = todos.splice(index, 1)
+            console.log(`OK, deleted ${deleted[0]}.`);
+        } else {
+            console.log("Unknown index")
+        }
+    }
+
+    input = prompt("What would you like to do?")
+}
+console.log("OK, QUIT THE APP")
